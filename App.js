5137d659d00a43434f2bd38d8400162a59cd02f4
Stephan D <stephan-mdd@gmail.com>
2018-11-13 10:07:01 +0100

    Added Navigation

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Platform, TouchableOpacity, StyleSheet } from 'react-native';
+import { Constants } from "expo";
+import { createStackNavigator  } from 'react-navigation';
 
-export default class App extends React.Component {
+import ListView from './components/ListView';
+
+const Touchable = (props) => (
+  <TouchableOpacity style={styles.button} onPress={props.onPress}>
+    <Text style={styles.buttonText}>{props.title}</Text>
+  </TouchableOpacity>)
+
+class HomeScreen extends React.Component {
+  static navigationOptions = { title: 'CA3 React Native' };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View >
+        <Text style={{ textAlign: "center", fontSize: 20 }}>Hello from newSlytherin</Text>
+        <Text style={{ textAlign: "center", fontSize: 20 }}>Hello from newSlytherin</Text>
+        <Touchable onPress={() => navigate('listView')} title="Lars" />
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+export default App = () => <RouteStack style={{ marginTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight / 2 }} />
+
+const RouteStack = createStackNavigator({
+  Home: { screen: HomeScreen },
+  listView: { screen: ListView },
 });
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 3,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 7,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: 'white'
+  }
+})
